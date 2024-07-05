6ecdc5f05979db9d504a4ebd0db16e5aa03d5d9b
Ajay Kumar <ajaykmr1501@gmail.com>
2024-07-06 00:03:30 +0530

    after adding full authorization on web

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,7 +26,12 @@ router.get('/new', isLoggedIn, catchAsync(async (req, res) => {
 
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews').populate('author');
+    const campground = await Campground.findById(id).populate({
+        path: 'reviews',
+        populate:{
+            path: 'author'
+        }
+    }).populate('author');
     if(!campground){
         req.flash('error','cannot find that campground')
         return res.redirect('/campgrounds');
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,14 +3,15 @@ const router = express.Router({mergeParams: true});
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const {reviewSchema} = require('../schemas.js');
-const {validateReview} = require('../middleware')
+const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
 const Review = require('../models/review');
 const Campground = require('../models/campground');
 
-router.post('/', validateReview,catchAsync(async (req,res)=>{
+router.post('/', isLoggedIn, isReviewAuthor, validateReview,catchAsync(async (req,res)=>{
     const { id } = req.params;
     const campground = await Campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -18,7 +19,7 @@ router.post('/', validateReview,catchAsync(async (req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:reviewId', catchAsync(async (req,res) =>{
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req,res) =>{
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id,{$pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(reviewId);
