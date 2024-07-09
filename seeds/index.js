23326f08a3f2e231a179e5fba146949c1490820a
Ajay Kumar <ajaykmr1501@gmail.com>
2024-07-09 22:38:34 +0530

    After Adding geometry in seed and update

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,15 @@
+if(process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const {places,descriptors} = require('./seedHelpers');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const { query } = require('express');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken: mapBoxToken});
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
@@ -22,9 +30,9 @@ const seedDb = async () =>{
         const camp = new Campground({
             author: '6685ae9efbf8fecbb27c2d38',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            geometry: { type: 'Point', 
-              coordinates: [ -122.330284, 47.603245 ]
-            },
+            // geometry: { type: 'Point', 
+            //   coordinates: [ -122.330284, 47.603245 ]
+            // },
             images: [
                 {
                   url: 'https://res.cloudinary.com/dzu9sfpdh/image/upload/v1720377330/YelpCamp/uonzpizlbo51k8vvpajv.jpg',
@@ -39,6 +47,11 @@ const seedDb = async () =>{
             description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque quae possimus aliquam quas accusantium amet esse eum iure fugit fugiat repellat',
             price
         })
+        const geoData = await geocoder.forwardGeocode({
+            query: camp.location,
+            limit: 1
+        }).send();
+        camp.geometry = geoData.body.features[0].geometry; 
         await camp.save();
     }
 }
