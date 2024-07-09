f98e7c3ef65878a6e7df4fcbd64e5a7f956d3f19
Ajay Kumar <ajaykmr1501@gmail.com>
2024-07-10 00:17:07 +0530

    Adding cluster map on campgronds page

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,8 @@
-if(process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
-}
-
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const {places,descriptors} = require('./seedHelpers');
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const { query } = require('express');
-const mapBoxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({accessToken: mapBoxToken});
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
@@ -24,15 +17,18 @@ const sample = arr => arr[Math.floor(Math.random()*arr.length)];
 
 const seedDb = async () =>{
     await Campground.deleteMany({});
-    for(let i = 0;i<50;i++){
+    for(let i = 0;i<300;i++){
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             author: '6685ae9efbf8fecbb27c2d38',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            // geometry: { type: 'Point', 
-            //   coordinates: [ -122.330284, 47.603245 ]
-            // },
+            geometry: { type: 'Point', 
+              coordinates: [
+                cities[random1000].longitude,
+                cities[random1000].latitude
+              ]
+            },
             images: [
                 {
                   url: 'https://res.cloudinary.com/dzu9sfpdh/image/upload/v1720377330/YelpCamp/uonzpizlbo51k8vvpajv.jpg',
@@ -47,11 +43,6 @@ const seedDb = async () =>{
             description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque quae possimus aliquam quas accusantium amet esse eum iure fugit fugiat repellat',
             price
         })
-        const geoData = await geocoder.forwardGeocode({
-            query: camp.location,
-            limit: 1
-        }).send();
-        camp.geometry = geoData.body.features[0].geometry; 
         await camp.save();
     }
 }
